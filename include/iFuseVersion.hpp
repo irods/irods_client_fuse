cf29c10c6bbf9e7055850fc53112c099fdd44041
Illyoung Choi <iychoi@email.arizona.edu>
2017-06-29 01:29:41 -0700

    Fix segfault when help is printed. Add iRodsFs version

---
+++ include/iFuseVersion.hpp
@@ -0,0 +1,10 @@
+/*** Copyright (c), The Regents of the University of California            ***
+ *** For more information please refer to files in the COPYRIGHT directory ***/
+/*** This code is rewritten by Illyoung Choi (iychoi@email.arizona.edu)    ***
+ *** funded by iPlantCollaborative (www.iplantcollaborative.org).          ***/
+#ifndef IFUSE_VERSION_HPP
+#define IFUSE_VERSION_HPP
+
+#define IFUSE_VERSION "2.1.1"
+
+#endif	/* IFUSE_VERSION_HPP */
